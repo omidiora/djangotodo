52f03de94e74521d8d18b92e1b0c6201db2615ae
omidiora <omidioraemmanuel@gmail.com>
2020-03-15 14:27:10 +0100

    templates

--- todo/urls.py
+++ todo/urls.py
@@ -2,4 +2,6 @@ from django.urls import path,include
 from todo import views
 urlpatterns = [
     path('',views.home, name='home'),
+    path('addtodo',views.add_todo),
+    path('delete_todo/<int:todo_id>',views.delete_todo)
 ]
